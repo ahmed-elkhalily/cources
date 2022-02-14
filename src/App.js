d5c4677b29aae0fcc320da5a29e04c4442ee92b0
a. Elkhalily <a.elkhalily10@gmail.com>
2022-02-15 01:14:35 +0200

    adding: react-router-dom, react-bootstrap,change-direction-to-rtl, sass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
+import { Row, Col, Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="border border-primary h-100">
+      <Row className="">
+        <Col className="border">
+          <div className="ms-4">أهلا وسهلا</div>
+        </Col>
+        <Col className="border">
+          <div className="ms-4">أهلا وسهلا</div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
